c294217596837a9d5a601506c71918c2b9e561bc
ADeleon13 <137022174+ADeleon13@users.noreply.github.com>
2023-12-27 20:26:20 -0600

    camera moverment

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,22 +9,55 @@ public class GameManager : MonoBehaviour
     public GameObject mainCamera;
 
     public GameObject marbleInstance;
-
+    public float moveSpeed;
+    public float sensitivity = 2;
     void Start()
     {
         marbleInstance = Instantiate(marblePrefab);
         marbleInstance.transform.position = spawnLocation.position;
-
+        Cursor.lockState = CursorLockMode.Locked;
 
         mainCamera = Camera.main.gameObject;
     }
 
     void Update()
     {
-        Vector3 newCameraPosition = marbleInstance.transform.position;
-        newCameraPosition.y += 10;
+        Vector3 newCameraPosition = mainCamera.transform.position;
+        float speedThisframe = moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+        {
+            newCameraPosition += mainCamera.transform.forward * speedThisframe;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            newCameraPosition += - mainCamera.transform.forward * speedThisframe;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newCameraPosition += - mainCamera.transform.right * speedThisframe;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newCameraPosition += mainCamera.transform.right  * speedThisframe;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newCameraPosition += mainCamera.transform.up * speedThisframe;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newCameraPosition += -mainCamera.transform.up * speedThisframe;
+        }
+
         mainCamera.transform.position = newCameraPosition;
 
-        mainCamera.transform.LookAt(marbleInstance.transform);
+        var rotation = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        var targetEuler = mainCamera.transform.rotation.eulerAngles + (Vector3)rotation * sensitivity;
+
+        targetEuler.x = Mathf.Clamp(targetEuler.x, -90, 90);
+        Quaternion newRotation = Quaternion.Euler(targetEuler);
+
+        mainCamera.transform.rotation = newRotation;
     }
 }
